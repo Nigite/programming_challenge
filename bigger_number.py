e0dbf2054d2fb9d2cf8a79701edb9cf01e22ef97
Niguel Compasivo <niguel.vvicente@gmail.comgit config --global user.name Nigite>
2026-03-11 12:40:52 +0800

    1-4 done

---
+++ bigger_number.py
@@ -0,0 +1,4 @@
+num_1 = float(input("Input first number: "))
+num_2 = float(input("Input second number: "))
+bigger_number = max(num_1, num_2)
+print(f"The bigger number is: {bigger_number}")
---
+++ sum_two_num.py
@@ -0,0 +1,3 @@
+num_1 = float(input("Input first number: "))
+num_2 = float(input("Input second number: "))
+print(f"The sum of two numbers are", num_1 + num_2)
